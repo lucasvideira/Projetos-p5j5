887b51a22d9ca395cb5de3b230312b96cfcee490
lucasvideira <videira@prof.educacao.sp.gov.br>
2024-05-07 13:22:50 -0300

    Repositório criado

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 400);
+  }
+  
+  function inicializaCores(){  //cria a função inicializaCores
+    background("rgb(170,81,81)");
+    fill("black");
+    textSize(64);
+    textAlign(CENTER,CENTER);
+  }
+  
+    function draw() {
+      inicializaCores();
+      
+
+    
